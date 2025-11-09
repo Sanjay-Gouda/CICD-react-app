c822e4a05bf922949df7b640650b5ca05dc7f860
Sanjay-Gouda <sanjayofficial755@gmail.com>
2025-11-09 12:17:53 +0530

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          CICD CHECK VERCEL {count}
+          COUNT CI/CD {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
